daefdf3b5d15716c9ff67f17950129c0787a54aa
Aayush Chalise <aayushchalise8@gmail.com>
2024-08-11 22:11:12 +0545

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-# Initial Setup file for new systems
-gitpath=`pwd`
-mkdir -p ~/.vim/undodir
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s $gitpath/.ignore $HOME/.ignore
+VIMRC_URL="https://raw.githubusercontent.com/aayushx402/MyVim/main/vimrc"
 
-# Share system clipboard with unnamedplus
-sudo apt install vim-gtk3 ripgrep fd-find xclip
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
 
-#ArchLinux
-pacman -S neovim-gtk
+install_editors() {
+    if ! command_exists vim && ! command_exists nvim; then
+        echo -e "\033[1;32mVim and Neovim are not installed. Installing...\033[0m"
+        sudo pacman -S vim neovim --noconfirm
+    else
+        echo -e "\033[1;32mVim or Neovim is already installed.\033[0m"
+    fi
+}
+
+apply_vimrc() {
+    echo -e "\033[1;32mDownloading and applying vimrc...\033[0m"
+    sudo curl -o /etc/vimrc "$VIMRC_URL"
+}
+
+echo -e "\033[1;34mStarting setup...\033[0m"
+
+if [ "$EUID" -ne 0 ]; then
+    echo -e "\033[1;33mRequesting sudo privileges...\033[0m"
+    sudo -v
+fi
+
+install_editors
+
+apply_vimrc
+
+echo -e "\033[1;32mConfiguration applied successfully!\033[0m"
+
